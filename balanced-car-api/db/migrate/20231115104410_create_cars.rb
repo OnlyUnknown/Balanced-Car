a7a8043323025732b9a5751ca10e934b3d9e64d9
OnlyUnknown <abdullahkhrief@gmail.com>
2023-11-15 13:54:54 +0300

    the proejct has been set up and the controller with the models has been set up

---
+++ balanced-car-api/db/migrate/20231115104410_create_cars.rb
@@ -0,0 +1,15 @@
+class CreateCars < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cars do |t|
+      t.string :name
+      t.string :tires_old
+      t.integer :oil
+      t.integer :auto_oil
+      t.text :note
+      t.integer :model
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
