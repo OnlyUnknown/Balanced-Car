38d7948bff52abbde4a5c4ce9e4f879f5d9a6819
OnlyUnknown <abdullahkhrief@gmail.com>
2023-12-20 17:01:52 +0300

    have created the sign in page

---
+++ balanced-car-front/src/components/signin.jsx
@@ -0,0 +1,13 @@
+import '../styling/signup.scss';
+
+const Signin = () => (
+  <div className="signup">
+    <h3 className="title">Sign In</h3>
+    <div className="shadow-box">
+      <input placeholder="Email" />
+      <input placeholder="Pass" />
+    </div>
+  </div>
+);
+
+export default Signin;
