8f07844b4ad2cdd1b4cace170335a2cad9376ace
OnlyUnknown <abdullahkhrief@gmail.com>
2023-12-24 20:11:01 +0300

    have added new component for the Car info page

---
+++ balanced-car-front/src/components/Carinfo.jsx
@@ -0,0 +1,7 @@
+import '../styling/Carinfo.scss';
+
+const Carinfo = () => (
+    <div></div>
+  );
+  
+  export default Carinfo;
---
+++ balanced-car-front/src/components/main.jsx
@@ -0,0 +1,7 @@
+import '../styling/main.scss';
+
+const Main = () => (
+<div></div>
+  );
+  
+  export default Main;
