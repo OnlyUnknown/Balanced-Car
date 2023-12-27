3166a0c4359cb954653497bea042bd38fa513b62
OnlyUnknown <abdullahkhrief@gmail.com>
2023-12-27 14:49:43 +0300

    have added the Carbox component

---
+++ balanced-car-front/src/components/CarComp.jsx
@@ -0,0 +1,14 @@
+import '../styling/CarComp.scss';
+
+const CarComp = () => (
+    <div className='box'>
+    <div>Pic</div>
+    <div className='info'>
+        <div>Oil change</div>
+        <div>Transimision Oil change</div>
+        <div>Tire age</div>
+    </div>
+    </div>
+  );
+
+export default CarComp
