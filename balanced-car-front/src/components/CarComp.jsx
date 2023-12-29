9c62dae459a14d1e70d58ceee17257f367f53fa5
OnlyUnknown <abdullahkhrief@gmail.com>
2023-12-29 17:01:03 +0300

    have added some styling for the car comp

--- balanced-car-front/src/components/CarComp.jsx
+++ balanced-car-front/src/components/CarComp.jsx
@@ -4,10 +4,10 @@ const CarComp = () => (
   <div className="box">
     <div className="pic">Pic</div>
     <div className="info">
-      <div>Oil change</div>
-      <div>TX Oil change</div>
-      <div>Tire age</div>
-      <div>Model</div>
+      <div>Oil change: 0000</div>
+      <div>TX Oil change: 0000</div>
+      <div>Tire age: 00</div>
+      <div>Model: 0000</div>
     </div>
   </div>
 );
