15f4e1dceef9c7aefd4678a3d81033b56775b0bf
OnlyUnknown <abdullahkhrief@gmail.com>
2024-01-01 18:14:25 +0300

    have created new component to add a car

---
+++ balanced-car-front/src/components/AddCar.jsx
@@ -0,0 +1,11 @@
+import '../styling/AddCar.scss';
+
+const AddCar = () => (
+  <div className="border">
+    <div className="carpic">Car pic</div>
+    <div className="carinfo">information</div>
+  </div>
+
+);
+
+export default AddCar;
