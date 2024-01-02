0fa38160a4d9adc5ba683e29cc1e202474548730
OnlyUnknown <abdullahkhrief@gmail.com>
2024-01-02 18:23:21 +0300

    have added the inputs for adding the car

--- balanced-car-front/src/components/AddCar.jsx
+++ balanced-car-front/src/components/AddCar.jsx
@@ -1,9 +1,19 @@
 import '../styling/AddCar.scss';
 
 const AddCar = () => (
-  <div className="border">
-    <div className="carpic">Car pic</div>
-    <div className="carinfo">information</div>
+  <div className="signup">
+    <h3 className="title">Sign In</h3>
+    <div className="shadow-box">
+      <input placeholder="Car Name" />
+      <input placeholder="Tansition type" />
+      <input placeholder="Model" />
+      <input placeholder="Transition Kilos" />
+      <input placeholder="Milage" />
+      <input placeholder="Auto Milage" />
+      <input placeholder="Tires age" />
+      <input placeholder="Oil Milage" />
+      <textarea placeholder="Note" />
+    </div>
   </div>
 
 );
