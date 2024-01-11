3ca01a44371499c5da6de0758ad78f6378dee699
OnlyUnknown <abdullahkhrief@gmail.com>
2024-01-11 12:54:40 +0300

    have added the routes for the components;

--- balanced-car-front/src/App.jsx
+++ balanced-car-front/src/App.jsx
@@ -1,15 +1,25 @@
 import './styling/App.scss';
+import './styling/AddCar.scss';
+
 // import Navigation from './Nav';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddCar from './components/AddCar';
 import Main from './components/main';
+import Carinfo from './components/Carinfo';
+import Splash from './components/splash';
+import Signup from './components/signup';
+import Signin from './components/signin';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" Component={Main}/>
-          <Route path="/addcar" Component={AddCar} />
+        <Route exact path="/" element={<Main />}/>
+          <Route path="/addcar" element={<AddCar />} />
+          <Route path="/splash" element={<Splash />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/singin" element={<Signin />} />
+          <Route path="/carinfo" element={<Carinfo />} />
       </Routes>
     </BrowserRouter>
   );
--- balanced-car-front/src/components/Carinfo.jsx
+++ balanced-car-front/src/components/Carinfo.jsx
@@ -1,11 +1,13 @@
 import '../styling/Carinfo.scss';
 
 const Carinfo = () => (
+  <>
+  <Navigation />
   <div className="border">
     <div className="carpic">Car pic</div>
     <div className="carinfo">information</div>
   </div>
-
+  </>
 );
 
 export default Carinfo;
