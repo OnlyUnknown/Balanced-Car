c6e69d915e0704f014b3aea0e9fb551b8713f118
OnlyUnknown <abdullahkhrief@gmail.com>
2024-05-26 16:08:27 +0300

    have added index cars and user models, and have added the following column for user: cars, and for cars: for_bidding, buy_limit and chassis_number, and changed the name of the tires_old to tires_age column

--- balanced-car-api/db/migrate/20231115104410_create_cars.rb
+++ balanced-car-api/db/migrate/20231115104410_create_cars.rb
@@ -1,8 +1,9 @@
 class CreateCars < ActiveRecord::Migration[7.0]
   def change
     create_table :cars do |t|
+      t.index :id, unique: true
       t.string :name
-      t.string :tires_old
+      t.string :tires_age
       t.integer :oil
       t.integer :auto_oil
       t.text :note
@@ -10,10 +11,13 @@ class CreateCars < ActiveRecord::Migration[7.0]
       t.references :user, null: false, foreign_key: true
       t.string :car_type
       t.string :transmission_type
+      t.boolean :for_bidding
       t.integer :last_bid
+      t.integer :buy_limit
       t.integer :revenue
       t.boolean :commercial
       t.boolean :public
+      t.string :chassis_number
       t.string :bills, array: true, default: []
 
       t.timestamps
