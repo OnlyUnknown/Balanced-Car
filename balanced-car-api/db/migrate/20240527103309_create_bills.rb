dceafff758fe6a0601c90985a32a61c9c8189851
OnlyUnknown <abdullahkhrief@gmail.com>
2024-05-28 16:09:25 +0300

    have created a total for the bills table

--- balanced-car-api/db/migrate/20240527103309_create_bills.rb
+++ balanced-car-api/db/migrate/20240527103309_create_bills.rb
@@ -2,6 +2,7 @@ class CreateBills < ActiveRecord::Migration[7.0]
   def change
     create_table :bills do |t|
       t.index :id, unique: true
+      t.float :total
       t.text :note
       t.references :car, null: false, foreign_key: true
 
