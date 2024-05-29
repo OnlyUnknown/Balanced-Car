410390d86bbb2711a9141f93e93fffe2e9fab829
OnlyUnknown <abdullahkhrief@gmail.com>
2024-05-29 18:33:29 +0300

    have installed the omniauth for google sign in

--- balanced-car-api/config/routes.rb
+++ balanced-car-api/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
       resources :cars, only: [:index, :show, :create, :destroy]
       resources :users, only: [:show, :create, :destroy]
     end
