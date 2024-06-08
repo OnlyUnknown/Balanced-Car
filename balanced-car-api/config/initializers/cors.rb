db42ffe7a610770c8e89a11a624b5c2dc4989d98
OnlyUnknown <abdullahkhrief@gmail.com>
2024-06-08 17:13:22 +0300

    have uninstalled the devise to reinstall to fix the errors

--- balanced-car-api/config/initializers/cors.rb
+++ balanced-car-api/config/initializers/cors.rb
@@ -5,12 +5,16 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      # Specify which origins are allowed to make CORS requests
+      origins 'localhost:3001', 'example.com'
+  
+      # Specify which resources are allowed and which methods and headers are supported
+      resource '/api/*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        expose: ['X-Total-Count', 'Link'], # Optional: specify which response headers are exposed to the browser
+        max_age: 86400 # Optional: specify how long the preflight request cache should last
+    end
+  end
