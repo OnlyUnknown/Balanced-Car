f5f8c17dc1947e08155d4baf9c53e31be0638cbc
OnlyUnknown <abdullahkhrief@gmail.com>
2024-06-11 17:19:20 +0300

    have uninstalled the devise gem

--- balanced-car-api/config/environments/development.rb
+++ balanced-car-api/config/environments/development.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
   # Devise Mailer
-  config.action_mailer.default_url_options = { host: 'localhost', port: 4000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
--- balanced-car-api/config/routes.rb
+++ balanced-car-api/config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      devise_for :users, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'signup'
-  },
-  controllers: {
-    sessions: 'api/v1/users/sessions',
-    registrations: 'api/v1/users/registrations'
-  }
-
   get '/current_user', to: 'api/v1/current_user#index'
 
       resources :cars, only: [] do
