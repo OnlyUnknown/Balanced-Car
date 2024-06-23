c653ce2a19fb9a1cc85402ef4718e5626fd774b0
OnlyUnknown <abdullahkhrief@gmail.com>
2024-06-23 16:35:34 +0300

    have created a function to show the cars by id when it is public

--- balanced-car-api/app/controllers/api/V1/cars_controller.rb
+++ balanced-car-api/app/controllers/api/V1/cars_controller.rb
@@ -1,9 +1,26 @@
 class Api::V1::CarsController < ApplicationController
   def index; end
 
-  def show; end
+  def show
+    @car = Car.find_by_id(params[:id])
+    check_publicity(@car.public)
+    if @car.for_bidding == false
+    render json: @car, :except=> [:public, :for_bidding, :last_bid, :buy_limit, :commercial, :revenues, :chassis_number]
+    else
+      render json: @car, :except=> [:public, :commercial, :chassis_number, :revenues]
+    end
+  end
+
 
   def create; end
 
   def destroy; end
+end
+
+private
+
+def check_publicity(car)
+  return if car == true
+
+  raise ActiveRecord::RecordNotDestroyed, 'It is not a public car or car not found found'
 end
--- balanced-car-api/config/routes.rb
+++ balanced-car-api/config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       devise_for :users
-      resources :cars, only: [] do
+      resources :user, only: [] do
         post 'create_car', to: 'users#create', on: :collection
         get 'index', to: 'users#index', on: :collection
         get 'show/:id', to: 'users#show', on: :collection
         patch 'update/:id', to: 'users#update_car', on: :collection
         delete 'delete/:id', to: 'users#delete_car', on: :collection
         patch 'publicity/:id', to: 'users#switch_publicity', on: :collection
+      end
+      resources :car, only: [] do
+        get 'show/:id', to: 'cars#show', on: :collection
       end
       resources :users, only: [:show, :create, :destroy]
     end
