0da3c4f1e0344b8a2446aabecd8d482150dab360
OnlyUnknown <abdullahkhrief@gmail.com>
2024-06-23 16:56:23 +0300

    have fixed the check publicity to tell a message if the car not found or not public

--- balanced-car-api/app/controllers/api/V1/cars_controller.rb
+++ balanced-car-api/app/controllers/api/V1/cars_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::CarsController < ApplicationController
 
   def show
     @car = Car.find_by_id(params[:id])
-    check_publicity(@car.public)
+    check_publicity(@car)
     if @car.for_bidding == false
     render json: @car, :except=> [:public, :for_bidding, :last_bid, :buy_limit, :commercial, :revenues, :chassis_number]
     else
@@ -20,7 +20,12 @@ end
 private
 
 def check_publicity(car)
-  return if car == true
+  
+  if car == nil || car.public == false
+    raise ActiveRecord::RecordNotDestroyed, 'It is not a public car or not found'
+    else
+      return if car.public == true
+  end
 
-  raise ActiveRecord::RecordNotDestroyed, 'It is not a public car or car not found found'
+  
 end
