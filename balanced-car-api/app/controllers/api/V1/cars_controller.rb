d49dccd24c80d791bbca46570d835b863850db23
OnlyUnknown <abdullahkhrief@gmail.com>
2024-06-24 18:16:07 +0300

    have fixed linters

--- balanced-car-api/app/controllers/api/V1/cars_controller.rb
+++ balanced-car-api/app/controllers/api/V1/cars_controller.rb
@@ -1,26 +1,19 @@
 class Api::V1::CarsController < ApplicationController
-
   def show
     @car = Car.find_by_id(params[:id])
     check_publicity(@car)
     if @car.for_bidding == false
-    render json: @car, :except=> [:public, :for_bidding, :last_bid, :buy_limit, :commercial, :revenues, :chassis_number]
+      render json: @car, except: %i[public for_bidding last_bid buy_limit commercial revenues chassis_number]
     else
-      render json: @car, :except=> [:public, :commercial, :chassis_number, :revenues]
+      render json: @car, except: %i[public commercial chassis_number revenues]
     end
   end
-
 end
 
 private
 
 def check_publicity(car)
-  
-  if car == nil || car.public == false
-    raise ActiveRecord::RecordNotDestroyed, 'It is not a public car or not found'
-    else
-      return if car.public == true
-  end
+  raise ActiveRecord::RecordNotDestroyed, 'It is not a public car or not found' if car.nil? || car.public == false
 
-  
+  nil if car.public == true
 end
