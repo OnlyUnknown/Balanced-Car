4e021ba8c85c8fe586c67f09ecffe251782eed6a
OnlyUnknown <abdullahkhrief@gmail.com>
2024-07-08 17:06:41 +0300

    have modified the authslice.js file and the store.js file

--- balanced-car-front/src/store.js
+++ balanced-car-front/src/store.js
@@ -1,12 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './features/auth/authSlice';
-import userReducer from './features/user/userSlice';
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from '../features/auth/authSlice'
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
-    user: userReducer,
-  },
-});
-
-export default store;
+    auth: authReducer
+  }
+})
+export default store
