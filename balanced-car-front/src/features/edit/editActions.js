c1ed56331348f9507dcc296f4b601e4fc287eb53
OnlyUnknown <abdullahkhrief@gmail.com>
2024-07-21 01:07:52 +0300

    have updated the edit actions.js file

---
+++ balanced-car-front/src/features/edit/editActions.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const backendURL = 'http://localhost:3001';
+
+export const editUser = createAsyncThunk(
+    'auth/edit',
+    async ({ name, phone_number }, { rejectWithValue }) => {
+      try {
+        const config = {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        };
+        const response = await axios.patch(
+          `${backendURL}/api/v1/users/update_profile`,
+          { name, phone_number },
+          config,
+        );
+        return response.data;
+      } catch (error) {
+        // return custom error message from backend if present
+        if (error.response && error.response.data.message) {
+          return rejectWithValue(error.response.data.message);
+        }
+        return rejectWithValue(error.message);
+      }
+    },
+  );
