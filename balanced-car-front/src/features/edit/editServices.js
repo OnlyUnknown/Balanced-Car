797763c76ac9c54d264829b8cb1255d438a8a88a
OnlyUnknown <abdullahkhrief@gmail.com>
2024-07-31 19:01:53 +0300

    have adjused the editService file

--- balanced-car-front/src/features/edit/editServices.js
+++ balanced-car-front/src/features/edit/editServices.js
@@ -1,25 +1,31 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
 
 export const editApi = createApi({
+  reducerPath: 'editApi',
   // Set the baseUrl for every endpoint below
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/api/v1' }),
+  baseQuery: fetchBaseQuery({  // base url of backend API
+    baseUrl: 'http://localhost:3001/',
+    // prepareHeaders is used to configure the header of every request and gives
+    // access to getState which we use to include the token from the store
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().edit.userToken;
+      if (token) {
+        // include token in req header
+        headers.set('authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getProfile: builder.query({
       // Will make a request like https://pokeapi.co/api/v2/pokemon/bulbasaur
       query: `/users/tokens/info`,
+      
     }),
-    prepareHeaders: (headers, { getState }) => {
-        const token = getState().edit.userToken;
-        if (token) {
-          // include token in req header
-          headers.set('authorization', `Bearer ${token}`);
-        }
-        return headers;
-      },
       
     updateProfile: builder.mutation({
       query: ({ patch }) => ({
-        url: `/users/update_profile`,
+        url: `/user/update_profile`,
         // When performing a mutation, you typically use a method of
         // PATCH/PUT/POST/DELETE for REST endpoints
         method: 'PATCH',
