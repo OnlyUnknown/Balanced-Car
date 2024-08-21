3f7f4123f68f6813ce646dbd146adef4b1f90a96
OnlyUnknown <abdullahkhrief@gmail.com>
2024-08-21 18:16:38 +0300

    have created DriverInfo.jsx page

---
+++ balanced-car-front/src/components/DriverInfo.jsx
@@ -0,0 +1,50 @@
+import '../styling/Carinfo.scss';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { showItem } from '../features/show/showActions';
+import Navigation from './Nav';
+import Spinner from './Spinner';
+/* eslint-disable */
+
+
+const DeiverInfo = () => {
+  const { item, loading, success } = useSelector((state) => state.show);
+  const dispatch = useDispatch();
+  let { cid } = useParams();
+
+  useEffect(() => {
+    const classname = "driver";
+    const id = cid;
+    dispatch(showItem({ classname, id }));
+  }, [dispatch, cid]);
+
+
+  return (
+    <>
+      <Navigation />
+      {success === true ? (
+        <div className="border">
+          <div className="carpic">Revenue pic</div>
+          <div className="carinfo">
+          <div>Id: {item.id}</div>
+            <div>
+              Total: {item.name}
+            </div>
+            <div>The car: <a href={`/carinfo/${item.car_id}`}>{item.car_id}</a></div>
+            <div>date: {item.date}</div>
+            <div>Note: {item.note}</div>
+          </div>
+        </div>
+      ) : (
+        loading === true ? (
+          <Spinner/>
+        ) : (
+          <div>empty</div>
+        )
+      )}
+    </>
+  );
+};
+
+export default DeiverInfo;
