344c370c3f44368fd57e6acd62c2387890362efe
OnlyUnknown <abdullahkhrief@gmail.com>
2024-12-22 16:15:05 +0300

    have added a new extension to make a sticky navbar

--- balanced-car-front/src/components/Main.js
+++ balanced-car-front/src/components/Main.js
@@ -1,10 +1,16 @@
 import '../styling/main.scss';
 import CarComp from './CarComp';
 import Navigation from './Nav';
+import Headroom from 'react-headroom';
 
 const Main = () => (
   <>
-    <Navigation />
+    
+    <Headroom>  
+      <div className='nav'>
+      <Navigation />
+      </div>
+      </ Headroom >
     <div className="main_box">
       <CarComp />
     </div>
