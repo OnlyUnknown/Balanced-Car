d662eac97e052716736cd1e9bdf371fae3264a5f
OnlyUnknown <abdullahkhrief@gmail.com>
2025-01-06 17:53:01 +0300

    have added a new action to create groups

--- balanced-car-front/src/features/add/addActions.js
+++ balanced-car-front/src/features/add/addActions.js
@@ -28,3 +28,29 @@ export const addItem = createAsyncThunk(
     }
   },
 );
+
+export const addGroup = createAsyncThunk(
+  'add/group',
+  async ({ item }, { getState, rejectWithValue }) => {
+    try {
+      const token = getState().auth.userToken; // Retrieve token from the state
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const data = await axios.post(
+        `${backendURL}/user/create_group`,
+        {item},
+        config,
+      );
+      return data;
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      }
+      return rejectWithValue(error.message);
+    }
+  },
+);
