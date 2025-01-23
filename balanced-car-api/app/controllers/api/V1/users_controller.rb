eb6c9e0a6c2187a0b930bde0f3a2ef83b8c78a72
OnlyUnknown <abdullahkhrief@gmail.com>
2025-01-23 17:34:50 +0300

    Have fixed the problem of the Adding new driver

--- balanced-car-api/app/controllers/api/V1/users_controller.rb
+++ balanced-car-api/app/controllers/api/V1/users_controller.rb
@@ -111,9 +111,7 @@ class Api::V1::UsersController < ApplicationController
               revenue_params
             end
     @item = resource.new(param)
-    if param != driver_params && @item.save 
-      render json: @item
-    elsif param == driver_params  && @item.car.driver.nil? && @item.save
+    if  @item.save 
       render json: @item
     else
       render json: { errors: @item.errors.full_messages }, status: :unprocessable_entity
@@ -162,6 +160,7 @@ end
 
 def driver_params
   @car = Car.find_by_id(params[:car_id]) if params[:car_id].present?
+  raise 'Car already has a driver' if @car&.driver.present?
   params.require(:item).permit(
     :name,
     :identification,
