e5606496f019d8d6b402c520b0fc9a544925b82d
OnlyUnknown <abdullahkhrief@gmail.com>
2025-02-25 18:42:17 +0300

    have updated the remove comp

--- balanced-car-front/src/components/CompsComponents/RemoveComp.jsx
+++ balanced-car-front/src/components/CompsComponents/RemoveComp.jsx
@@ -1,17 +1,32 @@
 import '../../styling/CarComp.scss';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useDispatch} from 'react-redux';
 import { removeItem } from '../../features/remove/removeActions';
 
 const RemoveComp = ({resource, cid}) => {
+  const { loading, errors, success } = useSelector((state) => state.remove);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (success === true) {
+      const Transfer = () => {
+        navigate(-1);
+      };
+      setTimeout(Transfer, 1000); // Pass Transfer as a function reference, not by invoking it
+    }
+  }, [success]);
+
+    useEffect(() => {
+      if (success)
+        toast.success('Item delted successfully');
+    }, [success]);
 
   return (
     <>
-    {console.log(resource)}
-      <button onClick={() => dispatch(removeItem({ id: cid, classname: resource }))}>
-        Remove
+      <button onClick={() => dispatch(removeItem({ id: cid, classname: resource }))} disabled={loading || success}>
+      {loading ? <Spinner /> : 'remove'}
       </button>
     </>
   );
