27d9d677b59e411cec450f9995baf1652e46788a
OnlyUnknown <abdullahkhrief@gmail.com>
2025-03-08 17:21:52 +0300

    have added the funcitonality to delete a group

---
+++ balanced-car-front/src/components/CompsComponents/RemoveGComp.jsx
@@ -0,0 +1,43 @@
+import '../../styling/CarComp.scss';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector} from 'react-redux';
+import { removeGroup } from '../../features/remove/removeActions';
+import { toast } from 'react-toastify';
+import Spinner from '../Spinner';
+
+const RemoveGComp = ({cid, page}) => {
+  const { removeLoading, removeError, removeSuccess } = useSelector((state) => state.remove);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (removeError) {
+      toast.error(removeError);
+    }
+  }, [removeError]);
+
+  useEffect(() => {
+    if (removeSuccess === true) {
+      const Transfer = () => {
+        navigate("/" + page);
+      };
+      setTimeout(Transfer, 1000); // Pass Transfer as a function reference, not by invoking it
+    }
+  }, [removeSuccess]);
+
+    useEffect(() => {
+      if (removeSuccess)
+        toast.success('Item deleted successfully');
+    }, [removeSuccess]);
+
+  return (
+    <>
+      <button onClick={() => dispatch(removeGroup({ id: cid }))} disabled={removeLoading }>
+      {removeLoading ? <Spinner /> : 'remove'}
+      </button>
+    </>
+  );
+};
+
+export default RemoveGComp;
--- balanced-car-front/src/components/InfoComponents/GroupCarShow.jsx
+++ balanced-car-front/src/components/InfoComponents/GroupCarShow.jsx
@@ -1,14 +1,21 @@
 import '../../styling/main.scss';
 import GroupCarComp from '../CompsComponents/GroupCarComp';
+import RemoveGComp from '../CompsComponents/RemoveGComp';
 import Navigation from '../Navigation';
+import { useParams } from 'react-router-dom';
 
-const GroupCarShow = () => (
-  <>
-    <Navigation />
-    <div className="main_box">
-      <GroupCarComp />
-    </div>
-  </>
-);
+const GroupCarShow = () => {
+  const { gid } = useParams();
+
+  return (
+    <>
+      <Navigation />
+      <div className="main_box">
+        <GroupCarComp />
+      </div>
+      <RemoveGComp cid={gid} page={"groups"} />
+    </>
+  );
+};
 
 export default GroupCarShow;
