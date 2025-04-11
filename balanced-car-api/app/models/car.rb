a2acbe99701cd8735fc01c7962f8f96442544b44
OnlyUnknown <abdullahkhrief@gmail.com>
2025-04-11 13:52:11 +0300

    Have fixed referencing the cars in the drivers

--- balanced-car-api/app/models/car.rb
+++ balanced-car-api/app/models/car.rb
@@ -3,7 +3,7 @@ class Car < ApplicationRecord
   belongs_to :user
   has_many :bills, dependent: :destroy
   has_many :revenues, dependent: :destroy
-  belongs_to :driver, optional: true
+  has_one :driver
   has_many :group_items, as: :item, dependent: :destroy
   has_many :groups, through: :group_items
 end
--- balanced-car-api/db/migrate/20240626140340_add_foreign_key_to_cars.rb
+++ balanced-car-api/db/migrate/20240626140340_add_foreign_key_to_cars.rb
@@ -1,6 +1,5 @@
 class AddForeignKeyToCars < ActiveRecord::Migration[7.0]
   def change
-    add_reference :cars, :driver, index: true
-    add_foreign_key :cars, :drivers
+    add_reference :cars, :driver, index: true, foreign_key: true, null: true
   end
 end
