f2fb7aebc6ef496861360635ad227e0b00bd188c
OnlyUnknown <abdullahkhrief@gmail.com>
2025-04-26 17:37:30 +0300

    Have added the Bills and Revenues to teh Groups so they have the ability to be added in a group

--- balanced-car-api/app/models/group.rb
+++ balanced-car-api/app/models/group.rb
@@ -3,22 +3,11 @@ class Group < ApplicationRecord
   has_many :group_items, dependent: :destroy
   has_many :cars, through: :group_items, source: :item, source_type: 'Car'
   has_many :drivers, through: :group_items, source: :item, source_type: 'Driver'
-
-  GROUP_TYPES = %w[cars drivers].freeze
+  has_many :Bills, through: :group_items, source: :item, source_type: 'Bill'
+  has_many :revenues, through: :group_items, source: :item, source_type: 'Revenue'
 
   validates :name, presence: true
   validates :group_type, presence: true, inclusion: { in: GROUP_TYPES }
   validate :validate_group_items_type
-
-  private
-
-  def validate_group_items_type
-    group_items.each do |group_item|
-      if group_type == 'cars' && group_item.item_type != 'Car'
-        errors.add(:base, 'This group can only contain cars.')
-      elsif group_type == 'drivers' && group_item.item_type != 'Driver'
-        errors.add(:base, 'This group can only contain drivers.')
-      end
-    end
-  end
+  validate :validate_group_items_uniqueness
 end
---
+++ balanced-car-front/src/components/EditComponents/EditGroup.jsx
@@ -0,0 +1,85 @@
+import '../../styling/AddCar.scss';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify'; // Assuming you're using react-toastify for notifications
+import { addGroup } from '../../features/add/addActions';
+import Navigation from '../Navigation';
+import Error from '../Error';
+import Spinner from '../Spinner';
+/* eslint-disable */
+const AddGroup = () => {
+  const { loading, errors, success } = useSelector((state) => state.add);
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (success === true) {
+      const Transfer = () => {
+        navigate('/groups');
+      };
+      setTimeout(Transfer, 1000); // Pass Transfer as a function reference, not by invoking it
+    }
+  }, [success]);
+
+  const submitForm = async (item) => {
+
+      dispatch(addGroup({ item }))
+      .unwrap()
+      .then(() => {
+        toast.success('Driver added successfully');
+      })
+      .catch((err = errors.message) => {
+        toast.error('Failed to add group. Please try again.');
+      });
+  };
+  return (
+    <>
+      <Navigation />
+      <form onSubmit={handleSubmit(submitForm)}>
+        <div className="addcar">
+          <h3 className="title">Add group</h3>
+          <div className="shadow-box-add">
+            <div className="car_box_pic">
+              Pic
+            </div>
+            <div className="info_box">
+              <input
+                id="name"
+                type="text"
+                {...register('name', { required: true })}
+                placeholder="Name of the Group"
+                disabled={success}
+              />
+              <input
+                type="text"
+                {...register('description')}
+                placeholder="The description of the group"
+                disabled={success}
+              />
+              <input
+                type="checkbox"
+                {...register('public')}
+                disabled={success}
+              />
+              <label htmlFor="public">The publicity of the group</label>
+              <select {...register('group_type', { required: true })} disabled={success}>
+                <option value="">Select Category</option>
+                <option value="cars">Cars</option>
+                <option value="drivers">Drivers</option>
+              </select>
+              <button type="submit" disabled={loading || success}>
+                {loading ? <Spinner /> : 'Create'}
+              </button>
+              {errors && <Error message={errors.message} />}
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default AddGroup;
