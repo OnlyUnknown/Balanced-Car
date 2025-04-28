8579fc3e24c2d684f8cca5545ae287666e1bf714
OnlyUnknown <abdullahkhrief@gmail.com>
2025-04-28 19:34:38 +0300

    have created Slices for the Drivers Bills and Revenues

---
+++ balanced-car-front/src/features/show/showBSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { showItem } from './showActions';
+
+const getUserToken = () => {
+  const userToken = localStorage.getItem('userToken');
+  return userToken ? JSON.parse(userToken) : null;
+};
+
+const initialState = {
+  loading: false,
+  userInfo: null,
+  userToken: getUserToken(),
+  error: null,
+  success: false,
+  bills: null,
+};
+
+const showBSlice = createSlice({
+  name: 'edit',
+  initialState,
+  reducers: {
+    setCredentials: (state, { payload }) => {
+      state.userInfo = payload;
+    },
+  },
+  extraReducers: {
+    // show item
+    [showItem.pending]: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    [showItem.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.bills = payload.data;
+      state.success = true;
+    },
+    [showItem.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    }
+  },
+});
+
+export default showBSlice.reducer;
